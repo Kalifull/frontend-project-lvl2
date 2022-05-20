83031098df616640d2b371f87ab95b7264ae8209
Alexey <kalifull@mail.ru>
2022-05-20 19:56:32 +0300

    configure arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
