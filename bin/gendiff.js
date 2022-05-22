4a82e98cf4ae1d604e508841633a1f77795d2455
Alexey <kalifull@mail.ru>
2022-05-22 22:57:42 +0300

    add genDiff between flat objects

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -7,6 +8,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .option('-f, --format <type>', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import fs from 'fs';
+import path from 'path';
+import _ from 'lodash';
+
+const getPathFile = (filepath) => path.resolve(process.cwd(), filepath);
+
+const getFileContent = (filepath) => {
+  const fileContent = fs.readFileSync(getPathFile(filepath), 'utf8');
+  return JSON.parse(fileContent);
+};
+
+const compareFileContent = (obj1, obj2, replacer = ' ', spaceCount = 2) => {
+  const uniqKeys = _.union(_.keys(obj1), _.keys(obj2));
+  const currentSpace = replacer.repeat(spaceCount);
+  const lines = _.sortBy(uniqKeys)
+    .map((key) => {
+      const key1 = _.has(obj1, key);
+      const key2 = _.has(obj2, key);
+      const value1 = obj1[key];
+      const value2 = obj2[key];
+      if (!key1) {
+        return [`+ ${key} ${value2}`];
+      }
+      if (!key2) {
+        return [`- ${key}: ${value1}`];
+      }
+      if (_.isEqual(value1, value2)) {
+        return [`  ${key}: ${value1}`];
+      }
+      if (!_.isEqual(value1, value2)) {
+        return [`- ${key}: ${value1}`, `+ ${key}: ${value2}`];
+      }
+      throw new Error(`Unknown order state: '${key}'!`);
+    })
+    .flat();
+  const currentLines = lines.map((line) => `${currentSpace}${line}`);
+  return ['{', ...currentLines, '}'].join('\n');
+};
+
+const getDiff = (filepath1, filepath2) => {
+  const firstFile = getFileContent(filepath1);
+  const secondFile = getFileContent(filepath2);
+  return compareFileContent(firstFile, secondFile);
+};
+
+export default getDiff;
