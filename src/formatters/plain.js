a6951c9d47be10dc3c96cb6c8f84f84f9f9ef928
Alexey <kalifull@mail.ru>
2022-05-27 00:33:50 +0300

    fix .eslintrc.yml

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -9,25 +9,24 @@ const stringify = (value) => {
 };
 
 const makePlain = (diff) => {
-  const iter = (tree, ancestor) =>
-    tree.flatMap((node) => {
-      const path = [...ancestor, node.name].join('.');
+  const iter = (tree, ancestor) => tree.flatMap((node) => {
+    const path = [...ancestor, node.name].join('.');
 
-      switch (node.type) {
-        case 'added':
-          return `Property '${path}' was added with value: ${stringify(node.value)}`;
-        case 'deleted':
-          return `Property '${path}' was removed`;
-        case 'unchanged':
-          return [];
-        case 'changed':
-          return `Property '${path}' was updated. From ${stringify(node.firstValue)} to ${stringify(node.secondValue)}`;
-        case 'nested':
-          return `${iter(node.children, [path]).join('\n')}`;
-        default:
-          throw new Error(`Type: ${node.type} is undefined`);
-      }
-    });
+    switch (node.type) {
+      case 'added':
+        return `Property '${path}' was added with value: ${stringify(node.value)}`;
+      case 'deleted':
+        return `Property '${path}' was removed`;
+      case 'unchanged':
+        return [];
+      case 'changed':
+        return `Property '${path}' was updated. From ${stringify(node.firstValue)} to ${stringify(node.secondValue)}`;
+      case 'nested':
+        return `${iter(node.children, [path]).join('\n')}`;
+      default:
+        throw new Error(`Type: ${node.type} is undefined`);
+    }
+  });
 
   const plainDiff = iter(diff, []);
   return [...plainDiff].join('\n');
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 
-const getIndent = (depth, replacer = ' ', firstIndent = 2, spaceCount = 4) =>
-  replacer.repeat(spaceCount * depth).slice(firstIndent);
+const getIndent = (depth, replacer = ' ', firstIndent = 2, spaceCount = 4) => (
+  replacer.repeat(spaceCount * depth).slice(firstIndent));
 
 const stringify = (data, depth) => {
   if (!_.isObject(data)) {
@@ -9,36 +9,35 @@ const stringify = (data, depth) => {
   }
 
   const lines = Object.entries(data).map(
-    ([key, value]) => `${getIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`
+    ([key, value]) => `${getIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`,
   );
 
   return ['{', ...lines, `${getIndent(depth)}  }`].join('\n');
 };
 
 const makeStylish = (diff) => {
-  const iter = (tree, depth) =>
-    tree.map((node) => {
-      const makeLine = (value, sign) => `${getIndent(depth)}${sign} ${node.name}: ${stringify(value, depth)}`;
+  const iter = (tree, depth) => tree.map((node) => {
+    const makeLine = (value, sign) => `${getIndent(depth)}${sign} ${node.name}: ${stringify(value, depth)}`;
 
-      switch (node.type) {
-        case 'added':
-          return makeLine(node.value, '+');
-        case 'deleted':
-          return makeLine(node.value, '-');
-        case 'unchanged':
-          return makeLine(node.value, ' ');
-        case 'changed':
-          return [`${makeLine(node.firstValue, '-')}`, `${makeLine(node.secondValue, '+')}`].join('\n');
-        case 'nested':
-          return `${getIndent(depth)}  ${node.name}: ${[
-            '{',
-            ...iter(node.children, depth + 1),
-            `${getIndent(depth)}  }`,
-          ].join('\n')}`;
-        default:
-          throw new Error(`Type: ${node.type} is undefined`);
-      }
-    });
+    switch (node.type) {
+      case 'added':
+        return makeLine(node.value, '+');
+      case 'deleted':
+        return makeLine(node.value, '-');
+      case 'unchanged':
+        return makeLine(node.value, ' ');
+      case 'changed':
+        return [`${makeLine(node.firstValue, '-')}`, `${makeLine(node.secondValue, '+')}`].join('\n');
+      case 'nested':
+        return `${getIndent(depth)}  ${node.name}: ${[
+          '{',
+          ...iter(node.children, depth + 1),
+          `${getIndent(depth)}  }`,
+        ].join('\n')}`;
+      default:
+        throw new Error(`Type: ${node.type} is undefined`);
+    }
+  });
 
   const stylishDiff = iter(diff, 1);
   return ['{', ...stylishDiff, '}'].join('\n');
